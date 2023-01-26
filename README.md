e51378c10643be0292aa0fee7fcc64d734f03621
imartin25 <ianmartinandres@gmail.com>
2023-01-26 19:55:43 -0300

    added comments and readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# OpenAIWhitelistBot
+This project is a Discord bot. The bot will take a message from a user and check if the provided wallet address is part of a whitelist.
+
+## Tech Used
+- Node.js
+- Discord.js
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+//Imports the dotenv package, discord.js package, fs module, and the setToken() method from the REST package
 import { config } from "dotenv"
 import { Client, GatewayIntentBits, Routes } from "discord.js"
 import { REST } from '@discordjs/rest'
 import * as fs from 'fs';
+
+//Configures environment variables from the .env file
 config()
 const BOT_TOKEN = process.env.BOT_TOKEN
 const CLIENT_ID = process.env.CLIENT_ID
 const GUILD_ID = process.env.GUILD_ID
 
+//Creates a new client object with the intents and gateway bits that the bot needs
 const client = new Client({
     intents: [
         GatewayIntentBits.MessageContent,
@@ -14,10 +18,14 @@ const client = new Client({
         GatewayIntentBits.GuildMessages
     ]
 })
+
+//Logs the client into the Discord server using the provided token
 client.login(BOT_TOKEN)
 
+//Logs the contents of a message to the console
 client.on("messageCreate", (msg) => { console.log(msg.content) })
 
+//Reads in the wallet json file and stores it in an array
 let wallets = []
 fs.readFile('./src/wallets.json', async function read(error, data) {
     if (error) {
@@ -26,8 +34,12 @@ fs.readFile('./src/wallets.json', async function read(error, data) {
     wallets = await JSON.parse(data)[0]
 })
 
+//Creates a new REST object with the provided token
 const rest = new REST({ version: "10" }).setToken(BOT_TOKEN)
 
+
+//When the client receives an interaction, it will check if the interaction is a chat input command.
+//If it is, it will check if the command name is "wl" and if so, it will check if the inputted wallet address is valid and if it is in the whitelist.
 client.on("interactionCreate", async (interaction) => {
     if (interaction.isChatInputCommand()) {
         console.log("Hey world, interaction is chat imput command!")
@@ -56,6 +68,7 @@ client.on("interactionCreate", async (interaction) => {
     }
 })
 
+//Updates the guild commands on the Discord server with the specified commands
 async function main() {
     const commands = [
         {
